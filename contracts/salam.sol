27b3fc80ff15e5b7b1118fc9f5cae82198fd0098
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-21 08:25:31 +0700

    persiapan smart contract

---
+++ contracts/salam.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+// setiap file harus dimulai dengan pernyataan lisensi dan versi solidity untuk dicompile
+contract Salam {
+    string salam = "Met pagi, makan bang";
+}
+// ini adalah kontrak sederhana yang bernama Salam
+// di dalam kontrak salam terdapat variable string bernama salam
+// compile terlebih dahulu sebelum kontrak dideploy
