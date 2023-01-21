b6829ba4137ed937f4fc1fb3071e744e480a9fe2
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-21 09:31:44 +0700

    constant

---
+++ contracts/constant.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Constants {
+    // konvensi pengkodean untuk variabel constant menggunakan huruf besar
+    address public constant MY_ADDRESS = 0x3309F5e85593006CCEe362a0AA23e84c00CDeb5b;
+    uint public constant MY_UINT = 69;
+}
+
+
+// constant adalah variabel yang tidak dapat dimodifikasi.
+
+// Nilainya sudah dikodekan dengan baik dan menggunakan constant dapat menghemat biaya gas.
