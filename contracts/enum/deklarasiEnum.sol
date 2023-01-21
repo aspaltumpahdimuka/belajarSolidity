61a837ca8ecdade2b6109377d2c6dd93814de6d3
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-21 11:48:19 +0700

    deklarasi enum

---
+++ contracts/enum/deklarasiEnum.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+// This is saved 'deklarasiEnum.sol'
+
+enum Status {
+    Pending,
+    Shipped,
+    Accepted,
+    Rejected,
+    Canceled
+}
