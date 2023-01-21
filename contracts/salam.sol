076372831561d5770f579aaafef545fd37c6f97c
aspaltumpahdimuka <bountygus@gmail.com>
2023-01-21 08:29:30 +0700

    persiapan smart contract

--- contracts/salam.sol
+++ contracts/salam.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8; // ini berarti versi yang digunakan minimal versi 0.8
 // setiap file harus dimulai dengan pernyataan lisensi dan versi solidity untuk dicompile
 contract Salam {
-    string salam = "Met pagi, makan bang";
+    string public salam = "Met pagi, makan bang";
 }
 // ini adalah kontrak sederhana yang bernama Salam
 // di dalam kontrak salam terdapat variable string bernama salam
 // compile terlebih dahulu sebelum kontrak dideploy
+// keyword public berarti variabel dapat diakses oleh smart contract lain atu aplikasi lain
